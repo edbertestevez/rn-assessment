c047906eca9a78d919b80859dd2f27c5f06b883b
Edbert Jason Estevez <ejestevez26@gmail.com>
2024-01-23 15:49:00 +0800

    feat: eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+};
