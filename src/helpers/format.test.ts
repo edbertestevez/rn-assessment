4d405cf16fef015723d8eda4a1017a6d15327b7e
Edbert Jason Estevez <ejestevez26@gmail.com>
2024-01-24 00:00:45 +0800

    tests: helpers

---
+++ src/helpers/format.test.ts
@@ -0,0 +1,59 @@
+import { formatOrdersWithCustomerInfo } from './format';
+import { Order, OrderStatus } from '../types/Order';
+import { mockCustomers } from '../utils/mockData';
+
+describe('formatOrdersWithCustomerInfo test cases', () => {
+  it('returns formatted order with valid and existing customer', () => {
+    const additionalInfo: Omit<Order, 'orderId' | 'customerId'> = {
+      items: [
+        {
+          itemId: 101,
+          itemName: 'Cheeseburger',
+          quantity: 2,
+          price: 9.99,
+        },
+      ],
+      totalPrice: 25.46,
+      taxFree: false,
+      status: OrderStatus.CLOSE,
+      timestamp: '2023-09-20T12:30:45Z',
+    };
+
+    const orders = [
+      // Valid customerId
+      {
+        orderId: 1,
+        customerId: 101,
+        ...additionalInfo,
+      },
+      // Invalid customerId
+      {
+        orderId: 2,
+        customerId: 999,
+        ...additionalInfo,
+      },
+    ];
+
+    const customers = mockCustomers;
+
+    const response = formatOrdersWithCustomerInfo(orders, customers);
+
+    // Correct length of orders after formatting
+    expect(response).toHaveLength(2);
+
+    const orderWithValidCustomer = response[0];
+    const orderWithInvalidCustomer = response[1];
+
+    // Should have customer name included in the response if customer is found
+    expect(orderWithValidCustomer).toEqual({
+      ...orders[0],
+      customerName: 'John Doe',
+    });
+
+    // Should return 'Unknown' customer name in the response if customer is not found
+    expect(orderWithInvalidCustomer).toEqual({
+      ...orders[1],
+      customerName: 'Unknown',
+    });
+  });
+});
--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -1,3 +1,4 @@
+import { Customer } from '../types/Customer';
 import { Order, OrderStatus } from '../types/Order';
 
 export const mockOrders: Order[] = [
@@ -56,5 +57,36 @@ export const mockOrders: Order[] = [
     taxFree: true,
     status: OrderStatus.OPEN,
     timestamp: '2023-09-20T13:15:20Z',
+  },
+];
+
+export const mockCustomers: Customer[] = [
+  {
+    customerId: 101,
+    customerName: 'John Doe',
+    email: 'johndoe@example.com',
+    phone: '123-456-7890',
+    address: '123 Main Street',
+  },
+  {
+    customerId: 102,
+    customerName: 'Jane Smith',
+    email: 'janesmith@example.com',
+    phone: '987-654-3210',
+    address: '456 Elm Avenue',
+  },
+  {
+    customerId: 103,
+    customerName: 'Mike Johnson',
+    email: 'mikejohnson@example.com',
+    phone: '555-123-4567',
+    address: '789 Oak Road',
+  },
+  {
+    customerId: 104,
+    customerName: 'Sarah Lee',
+    email: 'sarahlee@example.com',
+    phone: '111-222-3333',
+    address: '321 Maple Lane',
   },
 ];
