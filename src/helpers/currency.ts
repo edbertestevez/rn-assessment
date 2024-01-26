cfeeff5943140c9dc9e03e9c24edadde05b8468c
Edbert Jason Estevez <ejestevez26@gmail.com>
2024-01-26 08:14:27 +0800

    chore: sample jsdoc

--- src/helpers/currency.ts
+++ src/helpers/currency.ts
@@ -1,3 +1,11 @@
+/**
+ * Formats the numbers with USD currency
+ * @param { number } value
+ * @returns { string }
+ * @example
+ * // returns $2.00
+ * formatCurrency(2);
+ */
 export const formatCurrency = (value: number) =>
   new Intl.NumberFormat('en-US', {
     style: 'currency',
